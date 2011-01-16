509b8deb1aa07279ef166cfb3d4aa0c510daaee7
Iain Cambridge <wackiebackie@gmail.com>
2011-01-16 12:32:20 +0000

    Removed redunant static variables and replaced with associative arrays  to allow for better reuse of code.

--- library/Iain/Optimize/Concat.php
+++ library/Iain/Optimize/Concat.php
@@ -17,40 +17,36 @@ class Iain_Optimize_Concat {
 	 * @var
 	 */
 	
-	public static $scripts = array();
+	public static $rawFiles = array( 'scripts' => array(), 'styles' => array() );
 	
-	public static $styles = array();
+	public static $concatFiles = array( 'scripts' => array(), 'styles' => array() );
 	
-	public static $pageScripts = array();
-	
-	public static $pageStyles = array();
-	
-	public static $builds = array();
+	public static $builds = array( 'scripts' => array(), 'styles' => array() );
 	
 	
-	public static function addScript($controllers,$script){
+	public static function addFile($fileType,$controllers,$script){
 		
 		if ( !is_array($controllers) ){
 			$controllers = array($controllers);
 		}
 		
 		foreach($controllers as $controller ){
-			if ( !isset(self::$scripts[$controller]) ){
-				self::$scripts[$controller] = array();
+			if ( !isset(self::$rawFiles[$fileType][$controller]) ){
+				self::$rawFiles[$fileType][$controller] = array();
 			}
 			if ( !is_array($script) ){
-				self::$scripts[$controller][] = $script;
+				self::$rawFiles[$fileType][$controller][] = $script;
 			} else {
-				self::$scripts[$controller] = array_merge(self::$scripts[$controller],$script);
+				self::$rawFiles[$fileType][$controller] = array_merge(self::$rawFiles[$fileType][$controller],$script);
 			}
 		}
 		
 	}
 	
-	protected static function process( $files ){
+	protected static function process( $fileType ){
 		$buildFiles = array();
-		$builds = array();
-		foreach ( $files as $controller => $controllerFiles ){
+		self::$builds = array();
+		foreach ( self::$rawFiles[$fileType] as $controller => $controllerFiles ){
 			
 			foreach ( $controllerFiles as $file ){
 				if ( !isset($buildFiles[$file]) || !is_array($buildFiles[$file]) ){
@@ -60,31 +56,31 @@ class Iain_Optimize_Concat {
 			}
 			
 		}
-		$pageScripts = array();
+		
 		foreach( $buildFiles as $file => $controllerArray ){
 			
 			sort($controllerArray);
 			$key = implode("", $controllerArray);
 			
 			foreach ( $controllerArray as $controller ){
-				if ( !isset($pageScripts[$controller]) || !is_array($pageScripts[$controller]) ){
-					$pageScripts[$controller] = array();
+				if ( !isset(self::$concatFiles[$fileType][$controller]) || !is_array(self::$concatFiles[$fileType][$controller]) ){
+					self::$concatFiles[$fileType][$controller] = array();
 				}
-				$pageScripts[$controller][] = $key;	
+				self::$concatFiles[$fileType][$controller][] = $key;	
 			} 
 			
-			if ( !isset($builds[$key]) || !is_array($builds[$key]) ){
-				$builds[$key] = array();	
+			if ( !isset(self::$builds[$key]) || !is_array(self::$builds[$key]) ){
+				self::$builds[$key] = array();	
 			}
-			$builds[$key][] = $file;
+			self::$builds[$key][] = $file;
 		}
 		
-		return $builds;
+		return self::$builds;
 	}
 	
 	public static function getScripts(){
 
-		return self::process(self::$scripts);
+		return self::process('scripts');
 		
 	}
 	
--- tests/library/Iain_Optimize_ConcatTest.php
+++ tests/library/Iain_Optimize_ConcatTest.php
@@ -19,47 +19,47 @@ class Iain_Optimize_ConcatTest extends PHPUnit_Framework_TestCase
      */
     public function testAddScriptStringControllerStringScript ()
     {    	
-    	Iain_Optimize_Concat::$scripts = array();
-        Iain_Optimize_Concat::addScript('index','index.js');
-        $this->assertEquals(array('index') ,array_keys(Iain_Optimize_Concat::$scripts));
-        $this->assertEquals(array('index.js'), Iain_Optimize_Concat::$scripts['index'] );
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
+        Iain_Optimize_Concat::addFile('scripts','index','index.js');
+        $this->assertEquals(array('index') ,array_keys(Iain_Optimize_Concat::$rawFiles['scripts']));
+        $this->assertEquals(array('index.js'), Iain_Optimize_Concat::$rawFiles['scripts']['index'] );
     }
     
     public function testAddScriptArrayControllerStringScript (){
     	
-    	Iain_Optimize_Concat::$scripts = array();
-        Iain_Optimize_Concat::addScript(array('about','index'),'aboutindex.js');
-        $this->assertEquals(array('about','index') ,array_keys(Iain_Optimize_Concat::$scripts));
-        $this->assertEquals(array('aboutindex.js'), Iain_Optimize_Concat::$scripts['index'] );
-        $this->assertEquals(array('aboutindex.js'), Iain_Optimize_Concat::$scripts['about'] );
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
+        Iain_Optimize_Concat::addFile('scripts',array('about','index'),'aboutindex.js');
+        $this->assertEquals(array('about','index') ,array_keys(Iain_Optimize_Concat::$rawFiles['scripts']));
+        $this->assertEquals(array('aboutindex.js'),  Iain_Optimize_Concat::$rawFiles['scripts']['index'] );
+        $this->assertEquals(array('aboutindex.js'), Iain_Optimize_Concat::$rawFiles['scripts']['about'] );
         
     }
     
     public function testAddScriptStringControllerArrayScript() {
     	
-    	Iain_Optimize_Concat::$scripts = array();
-        Iain_Optimize_Concat::addScript('index',array('indexone.js','indextwo.js'));
-        $this->assertEquals(array('index') ,array_keys(Iain_Optimize_Concat::$scripts));
-        $this->assertEquals(array('indexone.js','indextwo.js'), Iain_Optimize_Concat::$scripts['index'] );
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
+        Iain_Optimize_Concat::addFile('scripts','index',array('indexone.js','indextwo.js'));
+        $this->assertEquals(array('index') ,array_keys(Iain_Optimize_Concat::$rawFiles['scripts']));
+        $this->assertEquals(array('indexone.js','indextwo.js'), Iain_Optimize_Concat::$rawFiles['scripts']['index'] );
     	
     }
     
     public function testAddScriptArrayControllerArrayScript() {
     	
-    	Iain_Optimize_Concat::$scripts = array();
-        Iain_Optimize_Concat::addScript(array('about','index'),array('aboutindexone.js','aboutindextwo.js'));
-        $this->assertEquals(array('about','index') ,array_keys(Iain_Optimize_Concat::$scripts));
-        $this->assertEquals(array('aboutindexone.js','aboutindextwo.js'), Iain_Optimize_Concat::$scripts['index'] );
-        $this->assertEquals(array('aboutindexone.js','aboutindextwo.js'), Iain_Optimize_Concat::$scripts['about'] );
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
+        Iain_Optimize_Concat::addFile('scripts',array('about','index'),array('aboutindexone.js','aboutindextwo.js'));
+        $this->assertEquals(array('about','index') ,array_keys(Iain_Optimize_Concat::$rawFiles['scripts']));
+        $this->assertEquals(array('aboutindexone.js','aboutindextwo.js'), Iain_Optimize_Concat::$rawFiles['scripts']['index'] );
+        $this->assertEquals(array('aboutindexone.js','aboutindextwo.js'), Iain_Optimize_Concat::$rawFiles['scripts']['about'] );
         
     }
     
     public function testBuildCombosTwoControllers(){
     	
-    	Iain_Optimize_Concat::$scripts = array();
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
     	
-    	Iain_Optimize_Concat::addScript('index', array('one.js','two.js'));
-    	Iain_Optimize_Concat::addScript('about', array('two.js','three.js'));
+    	Iain_Optimize_Concat::addFile('scripts','index', array('one.js','two.js'));
+    	Iain_Optimize_Concat::addFile('scripts','about', array('two.js','three.js'));
     	
     	$builds = Iain_Optimize_Concat::getScripts();
     	
@@ -80,11 +80,11 @@ class Iain_Optimize_ConcatTest extends PHPUnit_Framework_TestCase
     public function testBuildCombosThreeControllers(){
     	
     	
-    	Iain_Optimize_Concat::$scripts = array();
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
     	
-    	Iain_Optimize_Concat::addScript('index', array('index.js','aboutindex.js','aboutindexperson.js','indexperson.js'));
-    	Iain_Optimize_Concat::addScript('about', array('about.js','aboutindex.js','aboutindexperson.js','aboutperson.js'));
-    	Iain_Optimize_Concat::addScript('person', array('person.js','aboutperson.js','indexperson.js','aboutindexperson.js'));
+    	Iain_Optimize_Concat::addFile('scripts','index', array('index.js','aboutindex.js','aboutindexperson.js','indexperson.js'));
+    	Iain_Optimize_Concat::addFile('scripts','about', array('about.js','aboutindex.js','aboutindexperson.js','aboutperson.js'));
+    	Iain_Optimize_Concat::addFile('scripts','person', array('person.js','aboutperson.js','indexperson.js','aboutindexperson.js'));
     	
     	$builds = Iain_Optimize_Concat::getScripts();
     	
@@ -111,23 +111,23 @@ class Iain_Optimize_ConcatTest extends PHPUnit_Framework_TestCase
     
     public function testBuildCombosFourControllers(){
     	
-    	Iain_Optimize_Concat::$scripts = array();
+    	Iain_Optimize_Concat::$rawFiles = array( 'scripts' => array(), 'styles' => array() );
     	
- 		Iain_Optimize_Concat::addScript('about','about.js');		
- 		Iain_Optimize_Concat::addScript(array('about','index'),'aboutindex.js');
- 		Iain_Optimize_Concat::addScript(array('about','index','person'),'aboutindexperson.js');
- 		Iain_Optimize_Concat::addScript(array('about','index','picture'),'aboutindexpicture.js');
- 		Iain_Optimize_Concat::addScript(array('about','index','person','picture'),'aboutindexpersonpicture.js');
- 		Iain_Optimize_Concat::addScript(array('about','person'),'aboutperson.js');
- 		Iain_Optimize_Concat::addScript(array('about','person','picture'),'aboutpersonpicture.js');
- 		Iain_Optimize_Concat::addScript(array('about','picture'),'aboutpicture.js');
-    	Iain_Optimize_Concat::addScript('index','index.js');
- 		Iain_Optimize_Concat::addScript(array('index','person'),'indexperson.js');
- 		Iain_Optimize_Concat::addScript(array('index','person','picture'),'indexpersonpicture.js');
- 		Iain_Optimize_Concat::addScript(array('index','picture'),'indexpicture.js');
- 		Iain_Optimize_Concat::addScript('person','person.js');
- 		Iain_Optimize_Concat::addScript(array('person','picture'),'personpicture.js');
- 		Iain_Optimize_Concat::addScript('picture','picture.js'); 
+ 		Iain_Optimize_Concat::addFile('scripts','about','about.js');		
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','index'),'aboutindex.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','index','person'),'aboutindexperson.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','index','picture'),'aboutindexpicture.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','index','person','picture'),'aboutindexpersonpicture.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','person'),'aboutperson.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','person','picture'),'aboutpersonpicture.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('about','picture'),'aboutpicture.js');
+    	Iain_Optimize_Concat::addFile('scripts','index','index.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('index','person'),'indexperson.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('index','person','picture'),'indexpersonpicture.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('index','picture'),'indexpicture.js');
+ 		Iain_Optimize_Concat::addFile('scripts','person','person.js');
+ 		Iain_Optimize_Concat::addFile('scripts',array('person','picture'),'personpicture.js');
+ 		Iain_Optimize_Concat::addFile('scripts','picture','picture.js'); 
 		
  		
  		$expectedBuilds = array("about","aboutindex","aboutindexperson","aboutindexpersonpicture","aboutindexpicture",
