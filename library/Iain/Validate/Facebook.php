5f47a9e76f0afc63b1221b1dd7658fe81c442735
Iain Cambridge <wackiebackie@gmail.com>
2011-01-22 17:54:16 +0000

    Added Iain_Validate_Facebook

---
+++ library/Iain/Validate/Facebook.php
@@ -0,0 +1,34 @@
+<?php 
+
+	/**
+	 * A facebook url validator. Created to have a nicer 
+	 * and more elegant way of handling the different 
+	 * error message and check that the facebook account
+	 * actually exists.
+	 * 
+	 * @author Iain Cambridge
+	 * @license http://backie.org/copyright/bpl/ BPL
+     * @version 0.2
+     * @todo Improve!!
+     */
+
+class Iain_Validate_Facebook extends Zend_Validate_Regex {
+	
+	
+	public function __construct($pattern = false){
+		
+		// Hardcode the pattern since a twitter account can only be there.
+		parent::__construct("~http://(www\.)?facebook.com/([a-zA-Z0-9\_\-]+)~isU");
+		$this->_messageTemplates = array( parent::NOT_MATCH => "%value% is not a valid facebook URL.",
+										 parent::ERROROUS => "Shit went bad! Error report has been sent.",
+										 parent::INVALID => "Shit went bad! Error report been sent.");
+	}
+	
+	public function isValid($value){
+		
+		// TODO add check to validate twitter account exists.
+		return parent::isValid($value);
+		
+	}
+	
+}
